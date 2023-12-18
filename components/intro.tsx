77576be7cd67d1b00b574007fc4a47a32a08d5c8
Thanyapisit Buaprakong <nyman3527@gmail.com>
2023-12-19 04:11:17 +0700

    update template

--- components/intro.tsx
+++ components/intro.tsx
@@ -7,7 +7,7 @@ const Intro = () => {
         Meat Avatar 
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
+        เนื้อสัตว์บนโลกของผักที่จะเปลี่ยนมุมมองต่อพวกเรา{' '}
         <a
           href="https://nextjs.org/"
           className="underline hover:text-blue-600 duration-200 transition-colors"
