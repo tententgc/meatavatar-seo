4635e73f1df9a2234df95be68814a716f77428c6
Thanyapisit Buaprakong <nyman3527@gmail.com>
2023-12-19 03:52:02 +0700

    update template

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Container from '../components/container'
-import MoreStories from '../components/more-stories'
+
 import HeroPost from '../components/hero-post'
 import Intro from '../components/intro'
 import Layout from '../components/layout'
@@ -14,7 +14,7 @@ type Props = {
 
 export default function Index({ allPosts }: Props) {
   const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+  
   return (
     <>
       <Layout>
@@ -33,7 +33,6 @@ export default function Index({ allPosts }: Props) {
               excerpt={heroPost.excerpt}
             />
           )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
         </Container>
       </Layout>
     </>
