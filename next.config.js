1a1d2e3a7b37f17aa24aea33d7b24038786fb0fd
Thanyapisit Buaprakong <nyman3527@gmail.com>
2023-12-19 05:23:45 +0700

    update template

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
-images: {
-    domains: [
-        'meatavatar.tententgc.com',
-    ]
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+            },
+        ],
+    },
+};
+
+module.exports = nextConfig;
