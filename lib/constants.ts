027e048b395a599c84a097d7c39f78f2947c3916
Thanyapisit Buaprakong <nyman3527@gmail.com>
2023-12-19 14:49:40 +0700

    update template

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const EXAMPLE_PATH = 'blog-starter'
 export const CMS_NAME = 'Markdown'
 export const HOME_OG_IMAGE_URL =
-  'public/assets/blog/dynamic-routing/MeatAvatar.webp'
+  '/assets/blog/dynamic-routing/MeatAvatar.webp'
